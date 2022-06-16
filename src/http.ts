36e70114031898ee7643481d644a0e85f11e2ebc
joaogf <joao.g.f@icloud.com>
2022-06-16 10:50:42 -0300

    feat: mongo setup and creation of schemas

--- src/http.ts
+++ src/http.ts
@@ -2,10 +2,17 @@ import express from 'express'
 import { join } from 'node:path'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
+import mongoose, { ConnectOptions } from 'mongoose'
 
 const app = express()
 
 export const server = createServer(app)
+
+//docker run --name mongodb -p 27017:27017 -d -t mongo
+mongoose.connect('mongodb://localhost/rocket-socket', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+} as ConnectOptions )
 
 app.use(express.static(join(process.cwd(), 'public')))
 
